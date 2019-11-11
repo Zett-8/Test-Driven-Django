7a1e88b1ec1474faceb6c3be75348c0b534865a8
Zett-8 <kame0008@gmail.com>
2019-11-11 21:23:19 +0900

    finish

--- app/recipe/test/test_ingredient_api.py
+++ app/recipe/test/test_ingredient_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 
 from recipe.serializers import IngredientSerializer
 
@@ -89,3 +89,32 @@ class PrivateIngredientsApiTests(TestCase):
         res = self.client.post(INGREDIENT_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipes(self):
+        """ test filtering ingredients by those assigned to recipes """
+        ingredient1 = Ingredient.objects.create(
+            user=self.user,
+            name='mushroom'
+        )
+        Ingredient.objects.create(user=self.user, name='butter')
+
+        recipe1 = Recipe.objects.create(
+            user=self.user,
+            title='1',
+            time_minutes=20,
+            price=2.00,
+        )
+
+        recipe1.ingredients.add(ingredient1)
+
+        recipe2 = Recipe.objects.create(
+            user=self.user,
+            title='french flies',
+            time_minutes=10,
+            price=5.00
+        )
+
+        recipe2.ingredients.add(ingredient1)
+
+        res = self.client.get(INGREDIENT_URL, {'assigned_only': 1})
+        self.assertEqual(len(res.data), 1)
